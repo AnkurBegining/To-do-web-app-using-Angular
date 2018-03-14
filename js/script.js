ab7686f4d99f09fda62d57de57a23b97c2c07497
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-03-15 01:13:41 +0530

    fetched input is being shown in console

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 var appX=angular.module('mainApp', []);
 
 appX.controller('app', function($scope){
+	
+	$scope.tasks = [];
+	
 	$scope.searchEnter = function(){
 		// console.log(event.keyCode);
 		if(event.keyCode==13 || event.which==13){
-			// Make a function for getting the value of input
+			
 			$scope.addTask();
 		}
 	};
-	$scope.addTask() = function(){
-
+	$scope.addTask = function(){
+		$scope.tasks.push($scope.task);
+		console.log($scope.tasks);
 	};
 });
