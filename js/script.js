f447f1a63d6a8d18133e5e887d5a1c39f6239e8f
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-03-15 01:34:03 +0530

    clear functionality of input added

--- js/script.js
+++ js/script.js
@@ -6,13 +6,18 @@ appX.controller('app', function($scope){
 	
 	$scope.searchEnter = function(){
 		// console.log(event.keyCode);
+		/*
+			Check enter button is pressed or not and if string are non-empty or not
+		*/
 		if(event.keyCode==13 || event.which==13){
-			
-			$scope.addTask();
+			if ($scope.task != "") {
+				$scope.addTask();
+			}
 		}
 	};
 	$scope.addTask = function(){
 		$scope.tasks.push($scope.task);
 		console.log($scope.tasks);
+		$scope.task="";
 	};
 });
